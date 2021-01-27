a8443f691d4eeae44ad88d241ac13be748e672a0
ELLEN BJORKMAN <ELLEN881008@HOTMAIL.SE>
2021-01-27 17:13:29 +0100

    Changes in order model.

--- models/order.js
+++ models/order.js
@@ -1,19 +1,19 @@
 import mongoose from 'mongoose'
 
 const Order = new mongoose.model('Order', {
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
   products: [
     {
       productId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Product'
-      },
-      userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now
       },
       quantity: Number,
     }
