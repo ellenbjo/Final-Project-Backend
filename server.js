ed8d0ffe36f368f1624859f3000f47b3f0b58730
ELLEN BJORKMAN <ELLEN881008@HOTMAIL.SE>
2021-02-10 12:50:26 +0100

    Changed json respone in designer products endpoint.

--- server.js
+++ server.js
@@ -250,7 +250,7 @@ app.get('/designers/:id/products', async (req, res) => {
     if (designer) {
       const products = await Product.find({
         designer: mongoose.Types.ObjectId(designer.id)
-      }).populate('designer')
+      })
       res.json(products)
     } else {
       res.status(400).json({ error: 'Products from designer could not be found'})
